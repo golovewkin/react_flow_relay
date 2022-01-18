7639923eccd99abe5cd5a4364af707dd6cb40286
golovewkin <golovewkin@gmail.com>
2022-01-19 01:59:08 +0200

    base query works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,47 @@
 import "./App.css";
+import React from "react";
+import fetchGraphQL from "./helpers/fetchGraphQL";
 
 function App() {
+
+  // When the component mounts we'll fetch a repository name
+  React.useEffect( () => {
+    let isMounted = true;
+    
+    const loadData = async () => {
+      try {
+        let isMounted = true;
+        const response = await fetchGraphQL(`
+          query RepositoryNameQuery {
+            frontEnd {
+              id
+              name
+              skills {
+                edges {
+                  node {
+                    id
+                    name
+                  }
+                }
+              }
+            }
+          }
+        `);
+        if (!isMounted) {
+          return;
+        }
+        console.log(response);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    loadData()
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
   return (
     <div className="App">
       <header># To Do</header>
---
+++ frontend/src/helpers/fetchGraphQL.js
@@ -0,0 +1,18 @@
+async function fetchGraphQL(text, variables) {
+
+  const response = await fetch('http://localhost:4000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  });
+
+  const json = await response.json();
+  return json.data;
+}
+
+export default fetchGraphQL;
