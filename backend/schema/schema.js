8ddc2a2b910a47c343aa89d5a23875c9164d74fa
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2022-01-19 17:30:28 -0500

    fix(backend): give numeric Area id, sill -> skill
    If I remember correctly, this should fix the backend part of the project

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -56,7 +56,9 @@ const areaType = new GraphQLObjectType({
   name: "Area",
   interfaces: [nodeInterface],
   fields: () => ({
-    id: globalIdField(),
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+    },
     name: {
       type: GraphQLString,
     },
@@ -107,7 +109,7 @@ const skillMutation = mutationWithClientMutationId({
   mutateAndGetPayload: ({ skillName, areaId }) => {
     const newSkill = createSkill(skillName, areaId);
     return {
-      sillId: newSkill.id,
+      skillId: newSkill.id,
       areaId,
     };
   },
